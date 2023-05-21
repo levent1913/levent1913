e12bade13c08343efb5a0aa6bfa434ef5b086a2f
levent1913 <46969193+levent1913@users.noreply.github.com>
2023-05-21 17:53:05 +0300

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,83 +1,17 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-east-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-east-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-east-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-east-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-east-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-east-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-west-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-west-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-west-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-west-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-west-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-west-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-northeast-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-northeast-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-northeast-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-northeast-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-northeast-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-northeast-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-northeast-3 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-northeast-3.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-northeast-3.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-northeast-3.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ca-central-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ca-central-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ca-central-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ca-central-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-central-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-central-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-central-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-west-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-west-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-west-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-west-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-west-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-west-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-west-3.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-west-3.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-west-3.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-north-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-north-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-north-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.sa-east-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.sa-east-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.sa-east-1.amazonaws.com/myimage:latest
+aws iam create-role --role-name tefez --assume-role-policy-document file://trust-policy.json
 
 
-aws ecr get-login-password --region af-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.af-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.af-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.af-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-east-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-east-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-east-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-east-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-south-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-south-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-south-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-south-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-3 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-3.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-3.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-3.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-4 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-4.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-4.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-4.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-south-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-south-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-south-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-south-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-central-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-central-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-central-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-central-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region me-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.me-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.me-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.me-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region me-central-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.me-central-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.me-central-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.me-central-1.amazonaws.com/myimage:latest
+trust-policy
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
