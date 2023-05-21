31e6acd7e1e96226dd60d64cd73f39d1601f93b2
levent1913 <46969193+levent1913@users.noreply.github.com>
2023-05-21 17:58:10 +0300

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,10 +1,1 @@
-aws lambda create-function \
---function-name Arfio \
---role arn:aws:iam::818650170751:role/service-role/tefez \
---package-type Image \
---code ImageUri=818650170751.dkr.ecr.us-east-1.amazonaws.com/myimage:latest \
---timeout 180 \
---architectures x86_64 \
---memory-size 10240 \
---query 'FunctionArn' \
---region us-east-1
+aws iam create-role --role-name tefez --assume-role-policy-document file://trust-policy.json
