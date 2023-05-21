00cc3f36c0ec145300ea59aa63c3c762a79056d6
levent1913 <46969193+levent1913@users.noreply.github.com>
2023-05-21 18:26:38 +0300

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,83 +1,292 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-east-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-east-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-east-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-east-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-east-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-east-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-west-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-west-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-west-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.us-west-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.us-west-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.us-west-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-northeast-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-northeast-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-northeast-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-northeast-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-northeast-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-northeast-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-northeast-3 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-northeast-3.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-northeast-3.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-northeast-3.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ca-central-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ca-central-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ca-central-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ca-central-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-central-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-central-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-central-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-west-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-west-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-west-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-west-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-west-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-west-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-west-3.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-west-3.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-west-3.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-north-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-north-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-north-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.sa-east-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.sa-east-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.sa-east-1.amazonaws.com/myimage:latest
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.us-east-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region us-east-2
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.us-west-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region us-west-1
 
 
-aws ecr get-login-password --region af-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.af-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.af-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.af-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-east-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-east-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-east-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-east-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-south-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-south-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-south-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-south-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-3 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-3.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-3.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-3.amazonaws.com/myimage:latest
-aws ecr get-login-password --region ap-southeast-4 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.ap-southeast-4.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.ap-southeast-4.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.ap-southeast-4.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-south-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-south-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-south-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-south-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region eu-central-2 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.eu-central-2.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.eu-central-2.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.eu-central-2.amazonaws.com/myimage:latest
-aws ecr get-login-password --region me-south-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.me-south-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.me-south-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.me-south-1.amazonaws.com/myimage:latest
-aws ecr get-login-password --region me-central-1 | docker login --username AWS --password-stdin 818650170751.dkr.ecr.me-central-1.amazonaws.com/myimage
-docker tag image:latest 818650170751.dkr.ecr.me-central-1.amazonaws.com/myimage:latest
-docker push 818650170751.dkr.ecr.me-central-1.amazonaws.com/myimage:latest
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.us-west-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region us-west-2
+
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-south-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-south-1
+
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-southeast-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-southeast-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-southeast-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-southeast-2
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-northeast-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-northeast-1
+
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-northeast-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-northeast-2
+
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-northeast-3.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-northeast-3
+
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ca-central-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ca-central-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-central-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-central-1
+
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-west-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-west-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-west-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-west-2
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-west-3.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-west-3
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-north-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-north-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.sa-east-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region sa-east-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.af-south-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region af-south-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-east-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-east-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-south-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-south-2
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-southeast-3.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-southeast-3
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.ap-southeast-4.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region ap-southeast-4
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-south-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-south-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-south-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-south-2
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.eu-central-2.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region eu-central-2
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.me-south-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region me-south-1
+
+aws lambda create-function \
+--function-name Arfio \
+--role arn:aws:iam::818650170751:role/tefez \
+--package-type Image \
+--code ImageUri=818650170751.dkr.ecr.me-central-1.amazonaws.com/myimage:latest \
+--timeout 895 \
+--architectures x86_64 \
+--memory-size 10240 \
+--query 'FunctionArn' \
+--region me-central-1
